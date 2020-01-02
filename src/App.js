1a96164264c4ad3c41af04afb4cf7ad2ddaeeab9
Michael Dunlap <michaeldunlap@earthlink.net>
2020-01-02 11:31:32 -0800

    sexy up navigation

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './App.css';
 
-import Home from './components/Home/home.component'
+import Home from './pages/Home/home.page'
 
 function App() {
   return (
---
+++ src/pages/Home/home.page.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './home.styles.scss'
+
+import Directory from '../../components/Directory/directory.component'
+
+const Home = () => (
+  <div className='home'>
+    <Directory />
+  </div>
+)
+
+export default Home
