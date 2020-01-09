69256520ea8c0adbc656f5355757d52b3a41d5e8
Michael Dunlap <michaeldunlap@earthlink.net>
2020-01-09 08:04:16 -0800

    implement firebase feature to store authenticated users

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -16,9 +16,9 @@ export const createUserProfileDocument = async (userAuth, additionalData) => {
   if(!userAuth) return
 
   const userRef = firestore.doc(`users/${userAuth.uid}`)
-  const snapShot = await userRef.get()
+  const snapshot = await userRef.get()
 
-  if(!snapShot.exists) {
+  if(!snapshot.exists) {
     const { displayName, email } = userAuth
     const createdAt = new Date();
 
@@ -33,6 +33,8 @@ export const createUserProfileDocument = async (userAuth, additionalData) => {
       console.log('error creating user', e.message)
     }
   }
+
+  return userRef
 }
 
 firebase.initializeApp(config)
--- src/pages/SignInSignUp/sign-in-sign-up.page.jsx
+++ src/pages/SignInSignUp/sign-in-sign-up.page.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 
 import SignIn from '../../components/SignIn/sign-in.component'
+import SignUp from '../../components/SignUp/sign-up.component'
 
 import './sign-in-sign-up.styles.scss'
 
 const SignInSignUp = () =>
   <div className='sign-in-sign-up'>
     <SignIn />
+    <SignUp />
   </div>
 
 export default SignInSignUp
