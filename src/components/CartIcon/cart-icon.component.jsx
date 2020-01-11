6b0c88bc0a407a668844d4a1f2a3dcb88e3831b2
Michael Dunlap <michaeldunlap@earthlink.net>
2020-01-11 09:46:34 -0800

    update cartIcon to reflect number of items in cart by reducing cartItem.qty

--- src/components/CartIcon/cart-icon.component.jsx
+++ src/components/CartIcon/cart-icon.component.jsx
@@ -7,14 +7,20 @@ import { ReactComponent as ShoppingIcon } from '../../assets/shopping-bag.svg'
 
 import './cart-icon.styles.scss'
 
-const CartIcon = ({ toggleCartShow }) =>
+const CartIcon = ({ toggleCartShow, itemCount }) =>
   <div className='cart-icon' onClick={toggleCartShow}>
     <ShoppingIcon className='shopping-icon' />
-    <span className='item-count'>0</span>
+    <span className='item-count'>{itemCount}</span>
   </div>
 
 const mapDispatchToProps = dispatch => ({
   toggleCartShow: () => dispatch(toggleCartShow())
 })
 
-export default connect(null, mapDispatchToProps)(CartIcon)
+const mapStateToProps = ({ cart: { cartItems } }) => ({
+  itemCount: cartItems.reduce(
+    (accQty, cartItem) => accQty + cartItem.qty, 0
+  )
+})
+
+export default connect(mapStateToProps, mapDispatchToProps)(CartIcon)
