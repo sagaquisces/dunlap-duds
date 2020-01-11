9e7c8f734221f7a20b7369d18a314977f4b55c1c
Michael Dunlap <michaeldunlap@earthlink.net>
2020-01-11 12:47:37 -0800

    conditionally render empty cart message when there are no cart items

--- src/components/CartDropdown/cart-dropdown.component.jsx
+++ src/components/CartDropdown/cart-dropdown.component.jsx
@@ -13,9 +13,11 @@ const CartDropdown = ({ cartItems }) =>
   <div className='cart-dropdown'>
     <div className='cart-items'>
       {
+        cartItems.length ?
         cartItems.map(cartItem =>
           <CartItem key={cartItem.id} item={cartItem} />
-        )
+        ) :
+        <span className='empty-message'>Cart is empty.</span>
       }
     </div>
     <CustomButton>GO TO CHECKOUT</CustomButton>
