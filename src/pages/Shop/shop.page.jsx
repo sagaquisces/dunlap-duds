c8824f7f617470d626e9d9c0536fc17d31ce60c9
Michael Dunlap <michaeldunlap@earthlink.net>
2020-01-14 11:22:10 -0800

    add collection data to redux reducer

--- src/pages/Shop/shop.page.jsx
+++ src/pages/Shop/shop.page.jsx
@@ -1,30 +1,23 @@
 import React from 'react'
-
-import SHOP_DATA from './shop.data.js'
+import { connect } from 'react-redux'
+import { createStructuredSelector } from 'reselect'
 
 import CollectionPreview from '../../components/CollectionPreview/collection-preview.component'
 
-class Shop extends React.Component {
-  constructor(props) {
-    super(props)
+import { selectCollections } from '../../redux/shop/shop.selectors'
 
-    this.state = {
-      collection: SHOP_DATA
+const Shop = ({ collections }) =>
+
+  <div className='shop-page'>
+    {
+      collections.map(({ id, ...otherCollectionProps }) =>
+        <CollectionPreview key={id} {...otherCollectionProps} />
+      )
     }
-  }
+  </div>
 
-  render() {
-    const { collection } = this.state
-    return (
-      <div className='shop-page'>
-        {
-          collection.map(({ id, ...otherCollectionProps }) =>
-            <CollectionPreview key={id} {...otherCollectionProps} />
-          )
-        }
-      </div>
-    )
-  }
-}
+const mapStateToProps = createStructuredSelector({
+  collections: selectCollections
+})
 
-export default Shop
+export default connect(mapStateToProps)(Shop)
