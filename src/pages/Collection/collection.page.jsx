dccdc3753cd59335cd60f5a830c208a42d8aefef
Michael Dunlap <michaeldunlap@earthlink.net>
2020-01-15 22:00:22 -0800

    optimize shop data for faster lookup

--- src/pages/Collection/collection.page.jsx
+++ src/pages/Collection/collection.page.jsx
@@ -8,11 +8,18 @@ import { selectCollection } from '../../redux/shop/shop.selectors'
 import './collection.styles.scss'
 
 const Collection = ({ collection }) => {
-  console.log(collection)
+  const { title, items } = collection
 
   return (
     <div className='collection'>
-      <h2>Collection Page</h2>
+      <h2 className='title'>{ title }</h2>
+      <div className='items'>
+        {
+          items.map(item =>
+            <CollectionItem key={item.id} item={item} />
+          )
+        }
+      </div>
     </div>
   )
 }
