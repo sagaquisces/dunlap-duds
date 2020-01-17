cec143859ee36e9b4fd03406c4198cb40316cb8e
Michael Dunlap <michaeldunlap@earthlink.net>
2020-01-16 17:38:35 -0800

    add pkg to add test version of stripe payment processing, add message for what should be entered for test payment processing.

--- src/pages/Checkout/checkout.page.jsx
+++ src/pages/Checkout/checkout.page.jsx
@@ -3,6 +3,7 @@ import { connect } from 'react-redux'
 import { createStructuredSelector } from 'reselect'
 
 import CheckoutItem from '../../components/CheckoutItem/checkout-item.component'
+import StripeButton from '../../components/StripeButton/stripe-button.component'
 
 import { selectCartItems, selectCartTotal } from '../../redux/cart/cart.selectors'
 
@@ -36,6 +37,12 @@ const Checkout = ({ cartItems, total }) =>
     <div className='total'>
       <span>TOTAL: ${total}</span>
     </div>
+    <div className='test-warning'>
+      *Please use the following test credit card for payment.
+      <br />
+      4242 4242 4242 4242 - Exp: 01/20 - CVV: 123
+    </div>
+    <StripeButton price={total} />
   </div>
 
 const mapStateToProps = createStructuredSelector({
